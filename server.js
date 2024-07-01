1169b8583089255917e088ea57db1eb6fb223446
Yaseen KC <ymkcmpd@gmail.com>
2024-07-01 09:12:18 +0530

    package: socket.io

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
-const myUuid = uuidv4();
+const io = require("socket.io")(server);
+
 const app = express();
+const myUuid = uuidv4();
 
 app.use(express.static("public"));
-
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
@@ -13,6 +14,12 @@ app.get("/", (req, res) => {
 
 app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", () => {
+    console.log("Joined the room");
+  });
 });
 
 app.listen(3000, () => {
